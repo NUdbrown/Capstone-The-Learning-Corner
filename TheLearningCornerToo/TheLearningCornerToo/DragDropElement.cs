82174e03067ad7400107708ce5e93d8407a08c63
Dia Brown <dbrown1903@outlook.com>
2016-02-18 13:25:31 -0700

    trying to fix drag and drop

--- TheLearningCornerToo/TheLearningCornerToo/DragDropElement.cs
+++ TheLearningCornerToo/TheLearningCornerToo/DragDropElement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Kinect.Input;
 using Microsoft.Kinect.Toolkit.Input;
 using Microsoft.Kinect.Wpf.Controls;
 
@@ -10,18 +11,12 @@ namespace TheLearningCornerToo
     {
         bool IKinectControl.IsManipulatable
         {
-            get
-            {
-                return true;
-            }
+            get { return true; }
         }
 
         bool IKinectControl.IsPressable
         {
-            get
-            {
-                return false;
-            }
+            get { return false; }
         }
 
         IKinectController IKinectControl.CreateController(IInputModel inputModel, KinectRegion kinectRegion)
@@ -31,27 +26,42 @@ namespace TheLearningCornerToo
     }
 
     /// <summary>
-    /// This controller listens to kinect manipulation events associated with an element
-    /// and changes state on the element accordingly.
+    ///     This controller listens to kinect manipulation events associated with an element
+    ///     and changes state on the element accordingly.
     /// </summary>
     public class DragDropElementController : IKinectManipulatableController
     {
+        private DragDropElement dragDropElement;
+
+        private ManipulatableModel inputModel;
+        private KinectRegion kinectRegion;
+
         public DragDropElementController(IInputModel inputModel, KinectRegion kinectRegion)
         {
             this.inputModel = inputModel as ManipulatableModel;
             this.kinectRegion = kinectRegion;
-            this.dragDropElement = this.inputModel.Element as DragDropElement;
+            dragDropElement = this.inputModel.Element as DragDropElement;
 
             this.inputModel.ManipulationStarted += InputModel_ManipulationStarted;
             this.inputModel.ManipulationUpdated += InputModel_ManipulationUpdated;
             this.inputModel.ManipulationCompleted += InputModel_ManipulationCompleted;
         }
 
-        private void InputModel_ManipulationCompleted(object sender, Microsoft.Kinect.Input.KinectManipulationCompletedEventArgs e)
+        FrameworkElement IKinectController.Element
+        {
+            get { return inputModel.Element as FrameworkElement; }
+        }
+
+        ManipulatableModel IKinectManipulatableController.ManipulatableInputModel
+        {
+            get { return inputModel; }
+        }
+
+        private void InputModel_ManipulationCompleted(object sender, KinectManipulationCompletedEventArgs e)
         {
         }
 
-        private void InputModel_ManipulationUpdated(object sender, Microsoft.Kinect.Input.KinectManipulationUpdatedEventArgs e)
+        private void InputModel_ManipulationUpdated(object sender, KinectManipulationUpdatedEventArgs e)
         {
             var parentCanvas = dragDropElement.Parent as Canvas;
             if (parentCanvas != null)
@@ -63,40 +73,21 @@ namespace TheLearningCornerToo
                 if (double.IsNaN(x)) x = 0;
 
                 // delta values are 0.0 to 1.0, so we need to scale it to the number of pixels in the kinect region
-                var yDelta = delta.Y * this.kinectRegion.ActualHeight;
-                var xDelta = delta.X * this.kinectRegion.ActualWidth;
+                var yDelta = delta.Y*kinectRegion.ActualHeight;
+                var xDelta = delta.X*kinectRegion.ActualWidth;
 
                 Canvas.SetTop(dragDropElement, y + yDelta);
                 Canvas.SetLeft(dragDropElement, x + xDelta);
             }
         }
 
-        private void InputModel_ManipulationStarted(object sender, Microsoft.Kinect.Input.KinectManipulationStartedEventArgs e)
+        private void InputModel_ManipulationStarted(object sender, KinectManipulationStartedEventArgs e)
         {
         }
-
-        FrameworkElement IKinectController.Element
-        {
-            get
-            {
-                return this.inputModel.Element as FrameworkElement;
-            }
-        }
-
-        ManipulatableModel IKinectManipulatableController.ManipulatableInputModel
-        {
-            get
-            {
-                return this.inputModel;
-            }
-        }
-
-        private ManipulatableModel inputModel;
-        private KinectRegion kinectRegion;
-        private DragDropElement dragDropElement;
 
         #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
+
+        private bool disposedValue; // To detect redundant calls
 
         protected virtual void Dispose(bool disposing)
         {
@@ -107,13 +98,13 @@ namespace TheLearningCornerToo
                     // TODO: dispose managed state (managed objects).          
                 }
 
-                this.kinectRegion = null;
-                this.inputModel = null;
-                this.dragDropElement = null;
+                kinectRegion = null;
+                inputModel = null;
+                dragDropElement = null;
 
-                this.inputModel.ManipulationStarted -= InputModel_ManipulationStarted;
-                this.inputModel.ManipulationUpdated -= InputModel_ManipulationUpdated;
-                this.inputModel.ManipulationCompleted -= InputModel_ManipulationCompleted;
+                inputModel.ManipulationStarted -= InputModel_ManipulationStarted;
+                inputModel.ManipulationUpdated -= InputModel_ManipulationUpdated;
+                inputModel.ManipulationCompleted -= InputModel_ManipulationCompleted;
 
                 disposedValue = true;
             }
@@ -124,7 +115,7 @@ namespace TheLearningCornerToo
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
         }
-        #endregion
 
+        #endregion
     }
 }
