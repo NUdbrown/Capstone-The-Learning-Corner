5050523f0b0a0f4d6306c7aad79705da33175f73
Dia Brown <dbrown1903@outlook.com>
2016-02-19 13:29:51 -0700

    saving

--- TheLearningCornerToo/TheLearningCornerToo/ColorLesson.xaml.cs
+++ TheLearningCornerToo/TheLearningCornerToo/ColorLesson.xaml.cs
@@ -1,35 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
+using System.IO;
 using System.Media;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Ink;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Microsoft.Speech.AudioFormat;
-using Microsoft.Speech.Recognition;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Windows.Ink;
-using System.Windows.Media.Animation;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit.Input;
 using Microsoft.Kinect.Wpf.Controls;
 using Microsoft.Samples.Kinect.SpeechBasics;
-using TheLearningCornerToo.Pages;
-using HitTestResult = Microsoft.Kinect.Toolkit.Input.HitTestResult;
-
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
 
 namespace TheLearningCornerToo
 {
